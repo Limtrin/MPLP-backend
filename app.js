3cafe52a037b06d2348c6235267f05dde7a9c72a
Александр Федюнин <limonad13@gmail.com>
2020-12-12 23:40:54 +0400

    new fix

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 
 const app = express()
-const hostname = '127.0.0.1'
 
-var port = process.env.port || 3000
+const port = 8080
 
 app.get('/', (req, res) => {
     res.send('Погнали!')
 })
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
     console.log('running port' + port)
 })
